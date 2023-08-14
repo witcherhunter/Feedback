31b77274897004e95fde10f6e50cd7e23126d3d7
witcherhunter <amirhossinsh2@gmail.com>
2023-08-14 22:31:45 +0330

    lastVersion

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import { compareArraysAsSet } from '@testing-library/jest-dom/dist/utils'
+
+function Card({ children, reverse }) {
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+
+  return (
+    <div
+      className='card'
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+Card.defaultProps = {
+  reverse: false,
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+}
+
+export default Card
